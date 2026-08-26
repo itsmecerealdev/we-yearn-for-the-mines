49d4f89c3fc70f3f09f9f4d0a263d859638cc3a0
tayG-oss <taygiulls@gmail.com>
2026-08-25 23:08:54 -0700

    Prime testing update ig

--- src/main.cc
+++ src/main.cc
@@ -4,18 +4,37 @@
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/miller_rabin.hpp>
 using namespace boost::multiprecision;
+using namespace boost::random;
 using namespace std;
  
+cpp_int prime_test(cpp_int num) {
+	if (miller_rabin_test(num, 50)) {
+  		return num;
+     } else {
+		num += 2;
+		prime_test(num);
+	 }
+
+}
+
 int main() {
-	 //Two very large prime numbers
-	 cpp_int P = 0;
+	//Two very large prime numbers
+	cpp_int p = 0;
+	cpp_int q = 0;
 
-	cpp_int Q = 0;
+	mt19937 mt
+	uniform_int_distribution<cpp_int> ui(-(cpp_int(1) << 256), cpp_int(1)<<256);
+	for(unsigned i = 0; i < 5; i++)
+		ui(mt);
+	
+	
+	cpp_int p = 0;//prime_test();
+	cpp_int q = 0;//prime_test();
 
-	cpp_int T = (P-1) * (Q-1);
-	cpp_int N = P * Q;
-	const cpp_int E = 65537;
-	cpp_int D = pow(E, -1) % T;
-	cpp_int M = 0; //Email integer (Decrypting is pow(S, D) % N)
-	cpp_int S = pow(M,65537);
- }
+	cpp_int t = (p-1) * (q-1);
+	cpp_int n = p * q;
+	const cpp_int e = 65537;
+	cpp_int d = pow(e, -1) % T;
+	cpp_int m = 0; //Email integer (Decrypting is pow(S, D) % N)
+	cpp_int s = pow(m,65537);
+}
